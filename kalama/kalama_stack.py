0c80ac9bf55222853b05d3f895dec9add3ce6f08
EC2 Default User <ec2-user@ip-172-31-17-122.us-west-2.compute.internal>
2019-07-17 22:59:24 +0000

    Base CDK Python project

--- kalama/kalama_stack.py
+++ kalama/kalama_stack.py
@@ -5,5 +5,5 @@ class KalamaStack(core.Stack):
 
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
-
+ 
         # The code that defines your stack goes here
