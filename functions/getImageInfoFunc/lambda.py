b2751c22ae35a7f4385a8605b9fb90ea2f0e910e
Jerry <ec2-user@ip-172-31-17-122.us-west-2.compute.internal>
2019-08-01 02:25:27 +0000

    Image processing workflow

--- functions/getImageInfoFunc/lambda.py
+++ functions/getImageInfoFunc/lambda.py
@@ -2,13 +2,13 @@ import os
 import datetime
 
 def handler(event, context):
-    
+
     # Grab the interesting bits from the event
     key = event['key']
 
     head, tail = os.path.split(key)  
     name, extension = tail.rsplit('.',2)
-    
+
     return { 
         'name': name,
         'extension': extension, 
